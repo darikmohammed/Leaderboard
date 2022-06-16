5072bcb4fe0b82f77bd543f8fe3d9efe9a640feb
darikmohammed <r.m.darik@gmail.com>
2022-06-16 22:48:56 +0300

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './style.css';
-import Game from './modules/Game';
-import sort from './modules/Sort';
+import Game from './modules/Game.js';
+import sort from './modules/Sort.js';
 
 let gameId = '';
-const baseUrl =
-  'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
 const form = document.querySelector('#score-form');
 const refreshBtn = document.querySelector('#form-refresh');
 const scoresData = document.querySelector('.recent-score ul');
@@ -50,7 +49,7 @@ form.addEventListener('submit', async (e) => {
   const score = document.querySelector('#form-score');
   const response = await game.addNewGame(
     { user: name.value, score: score.value },
-    gameId
+    gameId,
   );
   name.value = '';
   score.value = '';
